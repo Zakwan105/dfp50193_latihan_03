6c47130b22130a6a9d176c27b3dfac30542843ae
Zakwan105 <khairulzakwan105@gmail.com>
2021-10-04 12:49:49 +0800

    sehingga operator

--- index.php
+++ index.php
@@ -1,28 +1,55 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Latihan 03</title>
 </head>
+
 <body>
     <h1>Asas PHP</h1>
 
-    <?php 
+    <?php
     // komen satu baris
     # komen satu baris
-  
+
+    /* 
+        komen banyak baris
+    */
 
 
     $namapelajar = 'Khairul Zakwan';
     echo "<h2>Hello $namapelajar</h2>"; #output
-    
+
     $hadir = false;
     $umur = 20;
     $nokp = '010510090069';
 
+
+    $x = 100; #integer
+    $y = '100' #integer
     ?>
 
+    <p>
+        <?php
+        echo 20 . 21;
+        ?>
+    </p>
+
+    <h2>
+        <?php
+        $bandar = 'Arau';
+        $baru = 'Bandar:';
+        for ($x = 0; $x < strlen($bandar); $x++) {
+            $baru .= $bandar[$x] . ' ';
+        }
+        echo $baru;
+        ?>
+
+    </h2>
+
 </body>
+
 </html>
